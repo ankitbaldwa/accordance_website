e24a0b5b70dff0f30c710945b6e6a7d3af50b263
ankitbaldwa <ankitbaldwa1992@gmail.com>
2021-06-12 14:22:43 +0530

    cites master added

--- templates/Admin/MstCities/edit.php
+++ templates/Admin/MstCities/edit.php
@@ -3,8 +3,37 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\MstCity $mstCity
  */
+$getStates_url = $this->Url->build(['prefix'=>'Admin', 'controller'=>'MstCities', 'action'=>'getStates']);
 ?>
-<div class="row">
+<?= $this->Form->create($mstCity, ['id'=> 'cities-form', 'novalidate'=>"novalidate"]) ?>
+<div class="form-group row">
+    <label class="col-form-label col-lg-3 col-sm-12">Country Name *</label>
+    <div class="col-lg-9 col-md-9 col-sm-12">
+        <?= $this->Form->control('mst_country_id', ['type'=> 'select','label'=>false,'options' => $mstCountries,'empty'=>'Select Country', 'class' => 'form-control','data-url'=>$getStates_url, 'placeholder'=>'Select Country Name', 'required'=> false]) ?>
+    </div>
+</div>
+<div class="form-group row">
+    <label class="col-form-label col-lg-3 col-sm-12">State Name *</label>
+    <div class="col-lg-9 col-md-9 col-sm-12">
+        <?= $this->Form->control('mst_state_id', ['type'=> 'select','label'=>false, 'options' => $mstStates, 'class' => 'form-control', 'placeholder'=>'Select State', 'empty'=>'Select State', 'required'=> false]) ?>
+    </div>
+</div>
+<div class="form-group row">
+    <label class="col-form-label col-lg-3 col-sm-12">City Name *</label>
+    <div class="col-lg-9 col-md-9 col-sm-12">
+        <?= $this->Form->text('city_name', ['class' => 'form-control', 'placeholder'=>'City Name', 'autocomplete'=>'off', 'label'=> false, 'required'=> false]) ?>
+    </div>
+</div>
+<div class="kt-form__actions">
+    <div class="row">
+        <div class="col-lg-9 ml-lg-auto">
+            <?= $this->Form->button('Submit',['type' => 'submit','escapeTitle' => false, 'class'=>'btn btn-pill btn-brand','id'=>'kt_city_submit']); ?> 
+            <button type="button" class="btn btn-pill btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
+<?= $this->Form->end() ?>
+<!-- <div class="row">
     <aside class="column">
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
@@ -32,4 +61,4 @@
             <?= $this->Form->end() ?>
         </div>
     </div>
-</div>
+</div> -->
